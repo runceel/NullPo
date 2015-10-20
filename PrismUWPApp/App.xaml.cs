ed4da282ac2287ebe467e3de4dbd45202e1fd1ab
Kazuki Ohta <k_ota28@hotmail.com>
2015-10-20 17:44:39 +0900

    initial commit

---
+++ PrismUWPApp/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Unity.Windows;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+
+namespace PrismUWPApp
+{
+    sealed partial class App : PrismUnityApplication
+    {
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
+        {
+            this.NavigationService.Navigate("Main", null);
+            return Task.CompletedTask;
+        }
+    }
+}
